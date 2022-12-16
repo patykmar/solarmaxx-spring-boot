b8a25635bd6e7c82794dd0a9b99615d1b407bed5
Ing. Martin Patyk <patyk.m@gmail.com>
2022-12-17 00:50:05 +0100

    added device constants and config

--- src/main/java/cz/patyk/solarmaxx/backend/dto/device/DeviceConstants.java
+++ src/main/java/cz/patyk/solarmaxx/backend/dto/device/DeviceConstants.java
@@ -1,7 +1,9 @@
 package cz.patyk.solarmaxx.backend.dto.device;
 
 public class DeviceConstants {
-    public static String PORT_STATUS_ON = "ON";
-    public static String PORT_STATUS_OFF = "OFF";
-    public static String PORT_STATUS_NA = "N/A";
+    public static final String PORT_STATUS_ON = "ON";
+    public static final String PORT_STATUS_OFF = "OFF";
+    public static final String PORT_STATUS_NA = "N/A";
+    public static final String DEVICE_TYPE_TASMOTA = "tasmota";
+    public static final String DEVICE_TYPE_SHELLY_PRO = "shelly-pro";
 }
