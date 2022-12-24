f74e0b18496803a66c90b45a0a720a68446ef473
Martin Patyk <79904068+patykmar@users.noreply.github.com>
2022-12-24 01:07:05 +0100

    specify relations between entities and add JPA config to database (#1)

--- src/main/java/cz/patyk/solarmaxx/backend/entity/Device.java
+++ src/main/java/cz/patyk/solarmaxx/backend/entity/Device.java
@@ -1,10 +1,16 @@
 package cz.patyk.solarmaxx.backend.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -14,11 +20,22 @@ public class Device {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ToString.Exclude
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private User user;
+
+    @ToString.Exclude
+    @JoinColumn(name = "device_type_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private DeviceType deviceType;
     private String name;
     private String ipAddress;
     private Short port;
     private Short outputCount;
+
+    @ToString.Exclude
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "device")
     private List<DeviceSchedule> deviceSchedules;
 }
--- src/main/java/cz/patyk/solarmaxx/backend/entity/Tariff.java
+++ src/main/java/cz/patyk/solarmaxx/backend/entity/Tariff.java
@@ -1,14 +1,27 @@
 package cz.patyk.solarmaxx.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 public class Tariff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ToString.Exclude
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private User user;
     private String name;
     private Long price;
