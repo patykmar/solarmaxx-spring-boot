35f71e0f327330ffaca7b528406c0c8d3e04eeec
Martin Patyk <martin.patyk@telia.no>
2022-12-31 02:10:31 +0100

    output id mapper created

--- src/main/java/cz/patyk/solarmaxx/backend/mapper/relay/type/output/ShellyProOutputIdMapper.java
+++ src/main/java/cz/patyk/solarmaxx/backend/mapper/relay/type/output/ShellyProOutputIdMapper.java
@@ -2,17 +2,33 @@ package cz.patyk.solarmaxx.backend.mapper.relay.type.output;
 
 import cz.patyk.solarmaxx.backend.dto.relay.output.RelayOutputDto;
 import cz.patyk.solarmaxx.backend.entity.Relay;
+import cz.patyk.solarmaxx.backend.mapper.relay.type.url.ShellyProUrlMapper;
+import cz.patyk.solarmaxx.backend.mapper.relay.type.url.UrlParameterMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ShellyProOutputIdMapper implements OutputIdMapperInterface {
+import static cz.patyk.solarmaxx.backend.dto.relay.type.RelayTypeConstants.SHALLY_PRO_DEFAULT_OUTPUT_ID;
+
+public class ShellyProOutputIdMapper extends OutputIdMapper {
+
+    public ShellyProOutputIdMapper(UrlParameterMapper urlParameterMapper, ShellyProUrlMapper shellyProUrlMapper) {
+        super(urlParameterMapper, shellyProUrlMapper);
+    }
+
     @Override
     public List<RelayOutputDto> getDeviceOutputs(Relay relay, boolean offlineMode) {
-        return null;
+        List<RelayOutputDto> relayOutputDtoList = new ArrayList<>();
+
+        for (byte i = SHALLY_PRO_DEFAULT_OUTPUT_ID; i < relay.getOutputCount(); i++) {
+            relayOutputDtoList.add(toRelayOutputDto(relay, i));
+        }
+        return relayOutputDtoList;
     }
 
     @Override
     public List<RelayOutputDto> getDeviceOutputs(Relay relay) {
         return getDeviceOutputs(relay, false);
     }
+
 }
--- src/main/java/cz/patyk/solarmaxx/backend/mapper/relay/type/output/TasmotaOutputIdMapper.java
+++ src/main/java/cz/patyk/solarmaxx/backend/mapper/relay/type/output/TasmotaOutputIdMapper.java
@@ -2,17 +2,33 @@ package cz.patyk.solarmaxx.backend.mapper.relay.type.output;
 
 import cz.patyk.solarmaxx.backend.dto.relay.output.RelayOutputDto;
 import cz.patyk.solarmaxx.backend.entity.Relay;
+import cz.patyk.solarmaxx.backend.mapper.relay.type.url.TasmotaUrlMapper;
+import cz.patyk.solarmaxx.backend.mapper.relay.type.url.UrlParameterMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TasmotaOutputIdMapper implements OutputIdMapperInterface {
+import static cz.patyk.solarmaxx.backend.dto.relay.type.RelayTypeConstants.TASMOTA_DEFAULT_OUTPUT_ID;
+
+public class TasmotaOutputIdMapper extends OutputIdMapper {
+
+    public TasmotaOutputIdMapper(UrlParameterMapper urlParameterMapper, TasmotaUrlMapper tasmotaUrlMapper) {
+        super(urlParameterMapper, tasmotaUrlMapper);
+    }
+
     @Override
     public List<RelayOutputDto> getDeviceOutputs(Relay relay, boolean offlineMode) {
-        return null;
+        List<RelayOutputDto> relayOutputDtoList = new ArrayList<>();
+
+        for (byte i = TASMOTA_DEFAULT_OUTPUT_ID; i <= relay.getOutputCount(); i++) {
+            relayOutputDtoList.add(toRelayOutputDto(relay, i));
+        }
+        return relayOutputDtoList;
     }
 
     @Override
     public List<RelayOutputDto> getDeviceOutputs(Relay relay) {
         return getDeviceOutputs(relay, false);
     }
+
 }
