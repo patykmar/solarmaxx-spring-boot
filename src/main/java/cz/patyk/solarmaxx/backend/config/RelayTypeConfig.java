edd74a1a0b56c5943b0eb6dff1ed592032894cd0
Martin Patyk <martin.patyk@telia.no>
2022-12-31 02:07:26 +0100

    created RelayTypeUrlPatternDto

--- src/main/java/cz/patyk/solarmaxx/backend/config/RelayTypeConfig.java
+++ src/main/java/cz/patyk/solarmaxx/backend/config/RelayTypeConfig.java
@@ -1,14 +1,14 @@
 package cz.patyk.solarmaxx.backend.config;
 
 import cz.patyk.solarmaxx.backend.dto.relay.type.RelayTypeConstants;
-import cz.patyk.solarmaxx.backend.dto.relay.type.url.RelayTypeUrlPattern;
+import cz.patyk.solarmaxx.backend.dto.relay.type.url.RelayTypeUrlPatternDto;
 import org.springframework.context.annotation.Bean;
 
 public class RelayTypeConfig {
 
     @Bean
-    public RelayTypeUrlPattern relayTypeUrlPattern() {
-        return RelayTypeUrlPattern.builder()
+    public RelayTypeUrlPatternDto relayTypeUrlPattern() {
+        return RelayTypeUrlPatternDto.builder()
                 .ip(RelayTypeConstants.RELAY_TYPE_URL_PATTERN_IP)
                 .id(RelayTypeConstants.RELAY_TYPE_URL_PATTERN_ID)
                 .port(RelayTypeConstants.RELAY_TYPE_URL_PATTERN_PORT)
