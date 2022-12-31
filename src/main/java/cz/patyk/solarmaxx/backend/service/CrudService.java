c81b65ace50a3b6116232714e0999da6cdccb63a
Martin Patyk <martin.patyk@telia.no>
2022-12-31 23:46:42 +0100

    use generic service for basic service methods

--- src/main/java/cz/patyk/solarmaxx/backend/service/CrudService.java
+++ src/main/java/cz/patyk/solarmaxx/backend/service/CrudService.java
@@ -1,19 +1,26 @@
 package cz.patyk.solarmaxx.backend.service;
 
 import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 /**
  * Specify methods for basic CRUD operations
+ *
  * @param <I> input DTO object
- * @param <R> return DTO object
+ * @param <O> DTO out object
  * @param <E> entity object
  */
-public interface CrudService<I, R, E> {
-    List<R> getAll(Pageable pageable);
-    R getOne(Long id);
+public interface CrudService<I, O, E> {
+    List<O> getAll(Pageable pageable);
+
+    O getOne(Long id);
+
     E getOneEntity(Long id);
-    R newItem(I dtoIn);
-    R editItem(I dtoIn, Long id);
+
+    O newItem(I dtoIn);
+
+    O editItem(I dtoIn, Long id);
+
     void deleteItem(Long id);
 }
