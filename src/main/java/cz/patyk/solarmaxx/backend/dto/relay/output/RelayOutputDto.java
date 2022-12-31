0f645f610e6a60af519306bed27a586de2beca3d
Martin Patyk <martin.patyk@telia.no>
2022-12-31 02:04:37 +0100

    relay output refactor

--- src/main/java/cz/patyk/solarmaxx/backend/dto/relay/output/RelayOutputDto.java
+++ src/main/java/cz/patyk/solarmaxx/backend/dto/relay/output/RelayOutputDto.java
@@ -1,11 +1,17 @@
 package cz.patyk.solarmaxx.backend.dto.relay.output;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RelayOutputDto {
-    private Long outputId;
-    private String outputStatus;
+    private Byte outputId;
+    private OutputStatus outputStatus;
     private String statusUrl;
     private String turnOnUrl;
     private String turnOffUrl;
--- src/main/java/cz/patyk/solarmaxx/backend/dto/relay/output/TasmotaRelayOutputDto.java
+++ src/main/java/cz/patyk/solarmaxx/backend/dto/relay/output/TasmotaRelayOutputDto.java
@@ -1,4 +1,9 @@
 package cz.patyk.solarmaxx.backend.dto.relay.output;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class TasmotaRelayOutputDto extends RelayOutputDto {
 }
