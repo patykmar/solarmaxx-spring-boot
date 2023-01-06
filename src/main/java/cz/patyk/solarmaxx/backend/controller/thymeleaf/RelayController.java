06bbf05622eac60ef1032ee0092d32ffed863733
Martin Patyk <79904068+patykmar@users.noreply.github.com>
2023-01-06 15:28:35 +0100

    Created ThymeleafAbstractController, ThymeleafControllerInterface and implement it in to RelayController (#7)

--- src/main/java/cz/patyk/solarmaxx/backend/controller/thymeleaf/RelayController.java
+++ src/main/java/cz/patyk/solarmaxx/backend/controller/thymeleaf/RelayController.java
@@ -1,54 +1,55 @@
 package cz.patyk.solarmaxx.backend.controller.thymeleaf;
 
+import cz.patyk.solarmaxx.backend.dto.in.RelayDtoIn;
 import cz.patyk.solarmaxx.backend.dto.out.RelayDtoOut;
 import cz.patyk.solarmaxx.backend.entity.Relay;
 import cz.patyk.solarmaxx.backend.service.RelayService;
 import cz.patyk.solarmaxx.backend.service.RelayTypeService;
 import cz.patyk.solarmaxx.backend.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Objects;
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/relay")
-public class RelayController {
-    private final RelayService relayService;
+public class RelayController extends ThymeleafAbstractController<RelayDtoIn, RelayDtoOut, Relay, Long> {
     private final RelayTypeService relayTypeService;
     private final UserService userService;
+
+    public RelayController(RelayService relayService, RelayTypeService relayTypeService, UserService userService) {
+        super(relayService, "redirect:/relay/list");
+        this.relayTypeService = relayTypeService;
+        this.userService = userService;
+    }
 
     @GetMapping({"", "/list"})
     public ModelAndView showGrid() {
         ModelAndView modelAndView = new ModelAndView("list/relay");
         PageRequest pageRequest = PageRequest.of(0, 10);
-        List<RelayDtoOut> relayServiceAll = relayService.getAll(pageRequest);
+        List<RelayDtoOut> relayServiceAll = service.getAll(pageRequest);
 
         modelAndView.addObject("relays", relayServiceAll);
         return modelAndView;
     }
 
-    @GetMapping("/detail")
-    public ModelAndView showDetail(@RequestParam Long id) {
+    @GetMapping("/detail/{id}")
+    public ModelAndView showDetail(@PathVariable Long id) {
         ModelAndView modelAndView = new ModelAndView("detail/relay");
-        RelayDtoOut relayDtoOut = relayService.getOne(id);
+        RelayDtoOut relayDtoOut = service.getOne(id);
 
         modelAndView.addObject("h1", relayDtoOut.getName());
         modelAndView.addObject("relayDtoOut", relayDtoOut);
         return modelAndView;
     }
 
-    @GetMapping({"/edit", "/new"})
-    public ModelAndView showForm(@RequestParam(required = false) Long id) {
+    @GetMapping({"/edit/{id}", "/new"})
+    public ModelAndView showForm(@PathVariable(required = false) Long id) {
         ModelAndView modelAndView = new ModelAndView("form/relay");
         Relay relayEntity;
         if (Objects.isNull(id)) {
@@ -56,7 +57,7 @@ public class RelayController {
             relayEntity = new Relay();
         } else {
             modelAndView.addObject("h1", "Edit relay");
-            relayEntity = relayService.getOneEntity(id);
+            relayEntity = service.getOneEntity(id);
         }
         modelAndView.addObject("relayTypeService", relayTypeService);
         modelAndView.addObject("userService", userService);
@@ -64,21 +65,4 @@ public class RelayController {
         return modelAndView;
     }
 
-    @PostMapping("/save")
-    public String save(@ModelAttribute Relay relay) {
-        if(Objects.isNull(relay.getId())){
-            // new item
-            relayService.newItemByEntity(relay);
-        } else {
-            // edit item
-            relayService.editItemByEntity(relay);
-        }
-        return "redirect:/relay/list";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id){
-        relayService.deleteItem(id);
-        return "redirect:/relay/list";
-    }
 }
