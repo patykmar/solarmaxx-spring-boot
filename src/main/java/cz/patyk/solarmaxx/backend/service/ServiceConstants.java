d295d4cc17b4c5621be74b7c587521d1db4369a4
Martin Patyk <martin.patyk@telia.no>
2023-01-07 22:24:44 +0100

    added RELAY_SCHEDULE_NOT_FOUND_MESSAGE

--- src/main/java/cz/patyk/solarmaxx/backend/service/ServiceConstants.java
+++ src/main/java/cz/patyk/solarmaxx/backend/service/ServiceConstants.java
@@ -8,4 +8,5 @@ public class ServiceConstants {
     public static final String USER_NOT_FOUND_MESSAGE = "User not found";
     public static final String RELAY_TYPE_NOT_FOUND_MESSAGE = "Relay type not found";
     public static final String RELAY_NOT_FOUND_MESSAGE = "Relay not found";
+    public static final String RELAY_SCHEDULE_NOT_FOUND_MESSAGE = "Relay schedule not found";
 }
