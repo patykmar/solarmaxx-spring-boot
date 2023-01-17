51244b4a900a55f4fd14e2483ebc243e1a516b5b
Martin Patyk <martin.patyk@telia.no>
2023-01-17 18:43:45 +0100

    added missing annotation FeignClient

--- src/main/java/cz/patyk/solarmaxx/backend/client/ShellyProClient.java
+++ src/main/java/cz/patyk/solarmaxx/backend/client/ShellyProClient.java
@@ -1,11 +1,14 @@
 package cz.patyk.solarmaxx.backend.client;
 
+import cz.patyk.solarmaxx.backend.config.RelayClientConfig;
 import feign.Headers;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.net.URI;
 
+@FeignClient(name = "shelly-pro-client", configuration = RelayClientConfig.class, url = "www.example.com")
 public interface ShellyProClient {
     @Headers("Content-Type: application/json")
     @GetMapping("/rpc/Switch.GetStatus?id={outputId}")
