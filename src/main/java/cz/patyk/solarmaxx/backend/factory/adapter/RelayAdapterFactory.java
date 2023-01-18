a76cdd99b23bb553c6c0370a9c467303152196b6
Martin Patyk <martin.patyk@telia.no>
2023-01-18 10:44:43 +0100

    renamed ShellyRelayAdapter to ShellyProRelayAdapter

--- src/main/java/cz/patyk/solarmaxx/backend/factory/adapter/RelayAdapterFactory.java
+++ src/main/java/cz/patyk/solarmaxx/backend/factory/adapter/RelayAdapterFactory.java
@@ -1,7 +1,7 @@
 package cz.patyk.solarmaxx.backend.factory.adapter;
 
 import cz.patyk.solarmaxx.backend.adapter.RelayAdapter;
-import cz.patyk.solarmaxx.backend.adapter.ShellyRelayAdapter;
+import cz.patyk.solarmaxx.backend.adapter.ShellyProRelayAdapter;
 import cz.patyk.solarmaxx.backend.adapter.TasmotaRelayAdapter;
 import cz.patyk.solarmaxx.backend.dto.relay.SupportedRelayType;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +11,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RelayAdapterFactory {
     private final TasmotaRelayAdapter tasmotaRelayAdapter;
-    private final ShellyRelayAdapter shellyRelayAdapter;
+    private final ShellyProRelayAdapter shellyProRelayAdapter;
 
     public RelayAdapter getRelayAdapter(SupportedRelayType relayType) {
         return switch (relayType) {
             case TASMOTA -> tasmotaRelayAdapter;
-            case SHELLY_PRO -> shellyRelayAdapter;
+            case SHELLY_PRO -> shellyProRelayAdapter;
         };
     }
 }
