f44d751817f8cc67ee0d24373dc2106066b22b13
Martin Patyk <martin.patyk@telia.no>
2023-01-18 10:36:21 +0100

    added information about IP

--- src/main/java/cz/patyk/solarmaxx/backend/adapter/RelayAdapter.java
+++ src/main/java/cz/patyk/solarmaxx/backend/adapter/RelayAdapter.java
@@ -4,9 +4,9 @@ import cz.patyk.solarmaxx.backend.dto.relay.output.RelayOutputDto;
 import lombok.NonNull;
 
 public interface RelayAdapter {
-    RelayOutputDto updateStatusFromRelay(@NonNull RelayOutputDto relayOutputDto);
+    RelayOutputDto updateStatusFromRelay(@NonNull RelayOutputDto relayOutputDto, String ip);
 
-    RelayOutputDto turnOnRelayOutput(@NonNull RelayOutputDto relayOutputDto);
+    RelayOutputDto turnOnRelayOutput(@NonNull RelayOutputDto relayOutputDto, String ip);
 
-    RelayOutputDto turnOffRelayOutput(@NonNull RelayOutputDto relayOutputDto);
+    RelayOutputDto turnOffRelayOutput(@NonNull RelayOutputDto relayOutputDto, String ip);
 }
