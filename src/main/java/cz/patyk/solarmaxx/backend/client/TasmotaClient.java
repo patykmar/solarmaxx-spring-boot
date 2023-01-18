065b8fb770190af8243a7fdfe23e80d6b8eacf67
Martin Patyk <martin.patyk@telia.no>
2023-01-18 10:38:42 +0100

    changed return type to TasmotaOutputDto

--- src/main/java/cz/patyk/solarmaxx/backend/client/TasmotaClient.java
+++ src/main/java/cz/patyk/solarmaxx/backend/client/TasmotaClient.java
@@ -1,6 +1,7 @@
 package cz.patyk.solarmaxx.backend.client;
 
 import cz.patyk.solarmaxx.backend.config.RelayClientConfig;
+import cz.patyk.solarmaxx.backend.dto.relay.output.TasmotaOutputDto;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,9 @@ import java.net.URI;
 
 @FeignClient(name = "tasmota-client", configuration = RelayClientConfig.class, url = "www.example.com")
 public interface TasmotaClient {
-
     @Headers("Content-Type: application/json")
     @GetMapping("/cm?cmnd=Power{outputId}%20STATUS")
-    String getOutputStatusWithSpecificPortObject(
+    TasmotaOutputDto getOutputStatusWithSpecificPortObject(
             URI baseUrl,
             @PathVariable("outputId") byte outputId
     );
