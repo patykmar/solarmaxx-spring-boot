7337b7a799f0892c73445b47cab7ca2b2c487ffa
Martin Patyk <martin.patyk@telia.no>
2023-01-25 22:25:08 +0100

    added missing service annotation

--- src/main/java/cz/patyk/solarmaxx/backend/service/WeekDayServices.java
+++ src/main/java/cz/patyk/solarmaxx/backend/service/WeekDayServices.java
@@ -3,9 +3,11 @@ package cz.patyk.solarmaxx.backend.service;
 import cz.patyk.solarmaxx.backend.dto.WeekDay;
 import cz.patyk.solarmaxx.backend.model.WeekDayModel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 @RequiredArgsConstructor
 public class WeekDayServices implements IWeekDay {
     private final WeekDayModel weekDayModel;
