dcd7a1f4d2a71ae1be107d2e998204817cb56f75
Ing. Martin Patyk <martin@patyk.cz>
2023-02-18 17:01:07 +0100

    removed un used class

--- src/main/java/cz/patyk/solarmaxx/backend/dto/relay/output/TasmotaRelayOutputDto.java
+++
@@ -1,9 +0,0 @@
-package cz.patyk.solarmaxx.backend.dto.relay.output;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class TasmotaRelayOutputDto extends RelayOutputDto {
-}
--- src/main/java/cz/patyk/solarmaxx/backend/mapper/relay/type/url/RelayTypeUrlMapper.java
+++
@@ -1,9 +0,0 @@
-package cz.patyk.solarmaxx.backend.mapper.relay.type.url;
-
-import cz.patyk.solarmaxx.backend.dto.relay.type.url.parameter.StatusUrlParameter;
-import cz.patyk.solarmaxx.backend.dto.relay.type.url.parameter.ToggleUrlParameter;
-
-public interface RelayTypeUrlMapper {
-    String toRealUrlStatus(StatusUrlParameter statusUrlParameter);
-    String toRealUrlToggle(ToggleUrlParameter toggleUrlParameter);
-}
