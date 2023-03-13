4abd2dd9f41a47b86ad4b425cc9050c582c7839f
Martin Patyk <mpatyk@deloittece.com>
2023-03-13 22:36:09 +0100

    update RelayOutput

--- src/main/java/cz/patyk/solarmaxx/backend/entity/RelayOutput.java
+++ src/main/java/cz/patyk/solarmaxx/backend/entity/RelayOutput.java
@@ -1,6 +1,7 @@
 package cz.patyk.solarmaxx.backend.entity;
 
 import cz.patyk.solarmaxx.backend.dto.relay.output.OutputStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -10,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +19,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -35,4 +39,8 @@ public class RelayOutput implements Serializable, IEntity<Long> {
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "relay_id", nullable = false)
     private Relay relay;
+    @ToString.Exclude
+    @Builder.Default
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true, mappedBy = "relayOutput")
+    private List<RelayOutputSchedule> relayOutputSchedules = new ArrayList<>();
 }
