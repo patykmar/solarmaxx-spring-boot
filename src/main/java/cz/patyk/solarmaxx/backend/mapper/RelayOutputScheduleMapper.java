4a5d241e89248aaa9800122bfec3a3c90c2ff2b1
Martin Patyk <martin@patyk.cz>
2023-03-18 23:16:54 +0100

    update RelayOutputScheduleMapper

--- src/main/java/cz/patyk/solarmaxx/backend/mapper/RelayOutputScheduleMapper.java
+++ src/main/java/cz/patyk/solarmaxx/backend/mapper/RelayOutputScheduleMapper.java
@@ -10,6 +10,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalTime;
+
 @Mapper(componentModel = "spring")
 public abstract class RelayOutputScheduleMapper implements BasicDataMapper<RelayOutputSchedule, RelayOutputScheduleDto, RelayOutputScheduleDataDto> {
     @Autowired
@@ -26,15 +28,34 @@ public abstract class RelayOutputScheduleMapper implements BasicDataMapper<Relay
     @Mapping(target = "dayNumber", expression = "java(getPositionFromWeekDay(dataDto.getWeekDay()))")
     public abstract RelayOutputSchedule dtoDataToEntity(RelayOutputScheduleDataDto dataDto);
 
+    @Override
+    @Mapping(target = "onStart", expression = "java(toLocalTime(entity.getOnStart()))")
+    @Mapping(target = "onEnd", expression = "java(toLocalTime(entity.getOnEnd()))")
+    @Mapping(target = "weekDay", expression = "java(toWeekDay(entity.getDayNumber()))")
+    @Mapping(target = "relayOutputId", source = "entity.relayOutput.outputId")
+    @Mapping(target = "relayOutputDescription", source = "entity.relayOutput.description")
+    @Mapping(target = "relayOutputStatus", source = "entity.relayOutput.outputStatus")
+    @Mapping(target = "relayId", source = "entity.relayOutput.relay.id")
+    public abstract RelayOutputScheduleDataDto entityToDataDto(RelayOutputSchedule entity);
+
+    @Override
+    @Mapping(target = "relayOutputId", source = "relayOutput.id")
+    public abstract RelayOutputScheduleDto entityToDto(RelayOutputSchedule entity);
+
     protected RelayOutput getRelayOutputById(Long id) {
         return relayOutputService.getOneEntity(id);
     }
 
-    protected WeekDayModel.WEEK_DAY getWeekDay(Byte dayNumber) {
+    protected WeekDayModel.WEEK_DAY toWeekDay(Byte dayNumber) {
         return weekDayMapper.fromPositionToWeekDayEnum(dayNumber);
     }
 
     protected Byte getPositionFromWeekDay(WeekDayModel.WEEK_DAY weekDay) {
         return weekDayMapper.toBytePositionFromWeekDayEnum(weekDay);
     }
+
+    protected LocalTime toLocalTime(String time) {
+        return LocalTime.parse(time);
+    }
+
 }
